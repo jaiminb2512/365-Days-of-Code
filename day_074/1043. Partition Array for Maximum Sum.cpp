de72cf29d1ff663ae2037ee8d4f57d7b4d81de41
jaiminb2512 <137876433+jaiminb2512@users.noreply.github.com>
2024-03-15 15:06:35 +0530

    Add files via upload

---
+++ day_074/1043. Partition Array for Maximum Sum.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        int n = arr.size();
+        vector<int> dp(n, 0);
+
+        for (int i = 0; i < n; ++i) {
+            int maxVal = 0;
+            for (int j = 1; j <= k && i - j + 1 >= 0; ++j) {
+                maxVal = max(maxVal, arr[i - j + 1]);
+                dp[i] = max(dp[i], (i >= j ? dp[i - j] : 0) + maxVal * j);
+            }
+        }
+
+        return dp[n - 1];
+    }
+};
---
+++ day_074/Search a 2D Matrix II.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h> 
+
+bool binarySearch(vector<int> &row, int target) {
+    int low = 0, high = row.size() - 1;
+    
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        
+        if (row[mid] == target) {
+            return true;
+        } else if (row[mid] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    
+    return false;
+}
+
+bool findInMatrix(int x, vector<vector<int>> &arr) {
+    int rows = arr.size();
+    int cols = arr[0].size();
+    
+    for (int i = 0; i < rows; ++i) {
+        
+        if (arr[i][0] <= x && x <= arr[i][cols - 1]) {
+            
+            if (binarySearch(arr[i], x)) {
+                return true;
+            }
+        }
+    }
+    
+    return false;
+}
