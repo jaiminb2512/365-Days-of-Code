48c1b860dcc7016ba4190e3b0d7021a54252d8c1
jaiminb2512 <137876433+jaiminb2512@users.noreply.github.com>
2024-03-18 12:10:51 +0530

    Add files via upload

---
+++ day_078/Search in a Linked List.cpp
@@ -0,0 +1,45 @@
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+    template <typename T>
+    class Node
+    {
+    public:
+        T data;
+        Node<T> *next;
+        Node()
+        {
+            this->data = 0;
+            this->next = NULL;
+        }
+        Node(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+        Node(T data, T* next)
+        {
+            this->data = data;
+            this->next = next;
+        }
+    };
+
+*****************************************************************/
+
+int searchInLinkedList(Node<int> *head, int k) { 
+    if (head == NULL)
+        return 0;
+
+    Node<int>* temp = head;
+
+    while(temp != NULL){
+        if(k == temp->data){
+            return 1;
+        }
+
+        temp = temp->next;
+    }
+
+    return 0;
+}
