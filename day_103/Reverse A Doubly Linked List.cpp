42352f845979cbd3237b1a786db5f76d60756b5a
jaiminb2512 <137876433+jaiminb2512@users.noreply.github.com>
2024-04-12 18:48:40 +0530

    Add files via upload

---
+++ day_103/Reverse A Doubly Linked List.cpp
@@ -0,0 +1,45 @@
+/*
+Following is the class structure of the Node class:
+
+class Node
+{
+public:
+    int data;
+    Node *next,*prev;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+        prev= NULL;
+    }
+    Node(int data)
+    {
+        this->data = data; 
+        this->next = NULL;
+        this->prev= NULL;
+    }
+    Node(int data, Node* next, Node *prev)
+    {
+        this->data = data;
+        this->next = next;
+        this->prev= prev;
+    }
+};
+
+*/
+
+Node* reverseDLL(Node* head)
+{   
+    Node* prev = nullptr;
+    Node* curr = head;
+
+    while(curr != nullptr){
+        Node* next = curr -> next;
+        curr->next = prev;
+        curr->prev = next; 
+        prev = curr;
+        curr = next;
+    }
+
+    return prev;
+}
