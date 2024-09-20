6c125515c68fca70552b6c28ae54b15dd6f6b18e
jaiminb2512 <137876433+jaiminb2512@users.noreply.github.com>
2024-09-20 15:56:16 +0530

    Add files via upload

---
+++ day_264/1934. Confirmation Rate.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT s.user_id, ROUND(AVG(if(c.action = "confirmed", 1, 0)), 2) AS confirmation_rate 
+    FROM Signups s
+    LEFT JOIN Confirmations c
+    ON s.user_id = c.user_id
+    GROUP BY user_id;
+    
