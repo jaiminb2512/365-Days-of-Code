cfe08073e2807cc3c90687597b77164d62a91cb9
jaiminb2512 <137876433+jaiminb2512@users.noreply.github.com>
2024-10-06 12:44:55 +0530

    Add files via upload

---
+++ day_278/1070. Product Sales Analysis III.sql
@@ -0,0 +1,8 @@
+SELECT s.product_id, s.year AS first_year, s.quantity, s.price
+    FROM Sales s
+    JOIN (
+            SELECT product_id, MIN(year) AS first_year
+            FROM Sales
+            GROUP BY product_id
+        ) AS first_sales
+    ON s.product_id = first_sales.product_id AND s.year = first_sales.first_year;
