a250abbd77ed7d3190bb30c9cf61dca90ebf0c45
jaiminb2512 <137876433+jaiminb2512@users.noreply.github.com>
2024-10-14 22:34:09 +0530

    Add files via upload

---
+++ day_288/1327. List the Products Ordered in a Period.sql
@@ -0,0 +1,13 @@
+SELECT 
+    p.product_name, 
+    SUM(o.unit) AS unit  
+FROM 
+    Products p
+LEFT JOIN  
+    Orders o ON p.product_id = o.product_id
+WHERE 
+    o.order_date BETWEEN '2020-02-01' AND '2020-02-29'
+GROUP BY 
+    p.product_name
+HAVING 
+    SUM(o.unit) >= 100;
