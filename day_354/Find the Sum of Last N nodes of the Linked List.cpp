89718e3c68d7de99cf5cf736f646a8f833373298
jaiminb2512 <137876433+jaiminb2512@users.noreply.github.com>
2024-12-22 09:10:53 +0530

    Add files via upload

---
+++ day_354/Find the Sum of Last N nodes of the Linked List.cpp
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    /*Structure of the node of the linled list is as
+
+    struct Node {
+        int data;
+        struct Node* next;
+
+        Node(int x){
+            data = x;
+            next = NULL;
+        }
+    };
+    */
+    // your task is to complete this function
+    // function should return sum of last n nodes
+    int sumOfLastN_Nodes(struct Node* head, int n) {
+        int len = 0;
+        
+        Node* temp = head;
+        
+        while(temp != nullptr){
+            len++;
+            temp = temp ->next;
+        }
+        
+        int des = len - n;
+        temp = head;
+        
+        while(des != 0){
+            temp = temp -> next;
+            des--;
+        }
+        
+        int ans = 0;
+        while(temp != nullptr){
+            ans += temp -> data;
+            temp = temp -> next;
+        }
+        
+        return ans;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int n;
+        cin >> n;
+        cin.ignore();
+        struct Node *head = new Node(arr[0]);
+        struct Node *tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+        Solution ob;
+        cout << ob.sumOfLastN_Nodes(head, n) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
